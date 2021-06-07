87bcb5d6937b03956cd24194bb0bedab4bc4e343
Akmadan23 <azadahmadi@mailo.com>
2021-06-07 11:04:18 +0200

    Fixing iframe responsive height

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function load(url) {
-    var iframe = document.getElementById("frame");
-    iframe.src = url;
+    document.getElementById("frame").src = url;
+}
+
+function setHeight(iframe) {
     iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
 }
