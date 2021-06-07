e9190865b286251bd9ae6d8459741d129a28eb8a
Akmadan23 <azadahmadi@mailo.com>
2021-06-07 09:05:32 +0200

    Complete renovation of the structure

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function show(x){
-    for (var i = 0; i < 3; i++) {
-        if (i != x) {
-            document.getElementById("main" + i).style="display: none;";
-            document.getElementById("menu" + i).style="display: none;";
-        }
-    }
-
-    document.getElementById("main" + x).style="display: block;";
-    document.getElementById("menu" + x).style="display: block;";
+function load(url) {
+    document.getElementById("frame").src = url;
 }
